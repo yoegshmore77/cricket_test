bf11c7fdae5ecc33a30a5c75f805c537de1550be
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2023-03-09 14:09:20 +0530

    Add files via upload

---
+++ game/mobile/timestamp.php
@@ -0,0 +1,8 @@
+<?php
+    echo milliseconds();
+
+    function milliseconds() {
+        $mt = explode(' ', microtime());
+        return ((int)$mt[1]) * 1000 + ((int)round($mt[0] * 1000));
+    }
+?>
